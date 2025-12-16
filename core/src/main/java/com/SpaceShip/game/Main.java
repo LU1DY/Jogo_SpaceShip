bd53ef78123a2e192adac0bb90fb9fa299d052fb
Luidy-Michael <luidymichael8@gmail.com>
2025-12-16 00:02:07 -0300

    feat: adicao de informacao de pontuacao, vidas e retorno ao jogo após derrota

--- core/src/main/java/com/SpaceShip/game/Main.java
+++ core/src/main/java/com/SpaceShip/game/Main.java
@@ -14,7 +14,9 @@ import com.badlogic.gdx.utils.TimeUtils;
 
 import java.sql.Time;
 import java.util.Iterator;
-
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Color;
 
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -23,11 +25,14 @@ public class Main extends ApplicationAdapter {
     private Texture image, tNave, tEnemy, tMissile;
     private Sprite nave, enemy, missile;
     private float posX, posY, velocity, xMissile, yMissile;
-    private boolean attack;
+    private boolean attack, gameOver;
     private Array<Rectangle> enemies;
     private long lastEnemyTime;
+    private int score, power, numEnemies;
 
-
+    private FreeTypeFontGenerator generator;
+    private FreeTypeFontGenerator.FreeTypeFontParameter parameter;
+    private BitmapFont bitmap;
 
     @Override
     public void create() {
@@ -49,6 +54,21 @@ public class Main extends ApplicationAdapter {
         enemies = new Array<Rectangle>();
 
         lastEnemyTime = 0;
+
+        score = 0;
+        power = 3;
+        numEnemies = 799999999;
+
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("font.ttf"));
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        parameter.size = 30;
+        parameter.borderWidth = 1;
+        parameter.borderColor = Color.BLACK;
+        parameter.color = Color.WHITE;
+        bitmap = generator.generateFont(parameter);
+
+        gameOver = false;
     }
 
     @Override
@@ -61,11 +81,34 @@ public class Main extends ApplicationAdapter {
         ScreenUtils.clear(1, 0, 0, 1);
         batch.begin();
         batch.draw(image, 0, 0);
-        batch.draw(missile, xMissile + nave.getWidth() / 2, yMissile + nave.getHeight() / 2 - 12);
-        batch.draw(nave, posX, posY);
-        for (Rectangle enemy : enemies) {
-            batch.draw(tEnemy, enemy.x, enemy.y);
+
+        if (!gameOver) {
+            if (attack) {
+                batch.draw(missile, xMissile, yMissile);
+            }
+            batch.draw(nave, posX, posY);
+
+            for (Rectangle enemy : enemies) {
+                batch.draw(tEnemy, enemy.x, enemy.y);
+            }
+            bitmap.draw(batch, "Score: " + score, 20, Gdx.graphics.getHeight() - 20);
+            bitmap.draw(batch, "Power: " + power, Gdx.graphics.getWidth() - 150, Gdx.graphics.getHeight() - 20);
+
+        } else  {
+            bitmap.draw(batch, "Score: " + score, 20, Gdx.graphics.getHeight() - 20);
+            bitmap.draw(batch, "GAME OVER!" + power, Gdx.graphics.getWidth() - 150, Gdx.graphics.getHeight() - 20);
+
+            if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+                score = 0;
+                power = 3;
+                posX = 0;
+                posY = 0;
+                gameOver = false;
+            }
         }
+
+
+
         batch.end();
     }
 
@@ -102,18 +145,18 @@ public class Main extends ApplicationAdapter {
     private void moveMissible() {
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !attack) {
             attack = true;
-            yMissile = posY;
+            yMissile = posY + nave.getHeight() / 2 - 12;
         }
         if (attack) {
             if (xMissile < Gdx.graphics.getWidth()) {
                 xMissile += 40;
             } else {
-                xMissile = posX;
+                xMissile = Gdx.graphics.getWidth();
                 attack = false;
             }
         } else {
-            xMissile = posX;
-            yMissile = posY;
+            xMissile = posX - nave.getHeight() / 2;
+            yMissile = posY + nave.getHeight() / 2 - 12;
         }
     }
 
@@ -124,16 +167,42 @@ public class Main extends ApplicationAdapter {
     }
 
     private void moveEnemies() {
-        if (TimeUtils.nanoTime() - lastEnemyTime > 1000000000) {
+        if (TimeUtils.nanoTime() - lastEnemyTime > numEnemies) {
             this.spawnEnemies();
         }
 
         for (Iterator<Rectangle> iter = enemies.iterator(); iter.hasNext();) {
             Rectangle enemy = iter.next();
             enemy.x -= 200 * Gdx.graphics.getDeltaTime();
+
+            // Colisão com o missil
+            if (collide(enemy.x, enemy.y, enemy.width, enemy.height, xMissile, yMissile, missile.getHeight(), missile.getHeight()) && attack){
+//                System.out.println("Score: " + ++score);
+                ++score;
+                if (score % 2 == 0) {
+                    numEnemies -= 100;
+                }
+                attack = false;
+                iter.remove();
+            // Colisão com a nave
+            } else if (collide(enemy.x, enemy.y, enemy.width, enemy.height, posX, posY, nave.getHeight(), nave.getHeight()) && !gameOver) {
+                if (power <= 0) {
+                    gameOver = true;
+                }
+                --power;
+                iter.remove();
+            }
+
             if (enemy.x + tEnemy.getHeight() < 0 ) {
                 iter.remove();
             }
         }
+    }
+
+    private boolean collide(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2) {
+        if (x1 + w1 > x2 && x1 < x2 + w2 && y1 + h1 > y2 && y1 < y2 + h2) {
+            return true;
+        }
+        return false;
     }
 }
